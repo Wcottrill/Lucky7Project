5f1b227f488552d9c2520205721a91824671faa2
Wesley Cottrill <WCottrill@my.gcu.edu>
2020-01-13 14:51:58 -0700

    Added Question 2 to Lucky 7

--- Lucky7project/Source.cpp
+++ Lucky7project/Source.cpp
@@ -6,35 +6,54 @@
 #include <iostream>
 using namespace std;
 
+//int main() {
+//	int a, b, c, d, e;
+//	cout << "Please Enter 5 Integers:";
+//	cin >> a >> b >> c >> d >> e;
+//
+//	int largest, smallest;
+//
+//
+//	if (a > b)largest = a;
+//	else largest = b;
+//
+//	if (c > largest)largest = c;
+//
+//	if (d > largest)largest = d;
+//
+//	if (e > largest)largest = e;
+//
+//	cout << "The largest number is:"<< largest << endl; 
+//
+//	if (a < b)smallest = a;
+//	else smallest= b;
+//
+//	if (c < smallest)smallest = c;
+//
+//	if (d < smallest)smallest = d;
+//
+//	if (e < smallest)smallest = e;
+//	cout << "The smallest number of the 5 is: " << smallest << endl;
+//
+//	//Write a program that reads in 5 integers and prints the largest and the smallest of the group.
+//	return 0;
+//}
+
+
+int calculate_sum(int a, int N){
+	int m = N / a;
+
+	int sum = m * (m + 1) / 2;
+
+	int ans = a * sum;
+
+	return ans;
+}
+
 int main() {
-	int a, b, c, d, e;
-	cout << "Please Enter 5 Integers:";
-	cin >> a >> b >> c >> d >> e;
-
-	int largest, smallest;
-
-
-	if (a > b)largest = a;
-	else largest = b;
-
-	if (c > largest)largest = c;
-
-	if (d > largest)largest = d;
-
-	if (e > largest)largest = e;
-
-	cout << "The largest number is:"<< largest << endl; 
-
-	if (a < b)smallest = a;
-	else smallest= b;
-
-	if (c < smallest)smallest = c;
-
-	if (d < smallest)smallest = d;
-
-	if (e < smallest)smallest = e;
-	cout << "The smallest number of the 5 is: " << smallest << endl;
-
-	//Write a program that reads in 5 integers and prints the largest and the smallest of the group.
+	int a = 7, N = 50;
+	cout << "Sum of multiples of  "
+		<< a << " up to " << N << " = "
+		<< calculate_sum(a, N) << endl;
 	return 0;
 }
